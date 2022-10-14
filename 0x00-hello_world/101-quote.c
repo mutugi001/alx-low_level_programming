48462e9408dda614efe887027a6937c86a65b42b
mutugi001 <collinsmut04@gmail.com>
2022-10-14 14:50:56 -0700

    printsto standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Description-'prints using writes'
+ * Return:Always 1 (Success)
+ */
+int main(void)
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
