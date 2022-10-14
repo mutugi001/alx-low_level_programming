9723f2e7d74d446a8e5646d0a9b868bebc13b4a4
mutugi001 <collinsmut04@gmail.com>
2022-10-14 13:49:55 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main-entry point
+ *Desciption:'printing out text'
+ *Return:Always 0 (success)
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
