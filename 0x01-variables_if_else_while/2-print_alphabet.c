23703c1b8f4eef5ee47462b9de939bc20e6b64bb
mutugi001 <collinsmut04@gmail.com>
2022-10-18 07:03:30 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description:'small alpha letters'
+ * Return:Always 0 (success)
+ */
+int main(void);
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar(ch);
+		ch++;
+	return (0);
+
+
+
+}
