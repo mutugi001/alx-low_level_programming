36aeeaf882445a22dd40befe0d623e0e29c128ea
mutugi001 <collinsmut04@gmail.com>
2022-10-18 11:49:47 -0700

    putchar1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main-Entry point
- * Return:Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
@@ -14,6 +14,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
