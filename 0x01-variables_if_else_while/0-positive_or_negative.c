c2db8257bd23716f12dba4f3cbf61f31e456a5f0
mutugi001 <collinsmut04@gmail.com>
2022-10-18 06:11:46 -0700

    random num

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main- Entry point
+ *Description: 'the program prints random numbers'
+ *Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d\n", n);
+	if (n > 0)
+		printf("is positive\n")
+	else if (n == 0)
+		printf("is zero\n")
+	else
+		printf("is negative\n")
+
+	return (0);
+
+}
