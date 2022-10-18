d2ffb1e39860eb97d29b7149ccc0128d693526bc
mutugi001 <collinsmut04@gmail.com>
2022-10-18 06:16:04 -0700

    random num

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("%d\n", n);
 	if (n > 0)
-		printf("is positive\n")
+		printf("is positive\n");
 	else if (n == 0)
-		printf("is zero\n")
+		printf("is zero\n");
 	else
-		printf("is negative\n")
+		printf("is negative\n");
 
 	return (0);
 
