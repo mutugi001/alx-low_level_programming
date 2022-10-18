a26083ae6db38ce43d963bb260f65dfc1e6bc413
mutugi001 <collinsmut04@gmail.com>
2022-10-18 07:38:09 -0700

    alphaletter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description : 'small and caps aplhas'
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	sl = "a"
+	cl = "A"
+
+	while (sl <= "z")
+		{
+		putchar(sl);
+		sl++;
+		}
+	while (cl <= "Z")
+		{
+		putchar(cl);
+		cl++;
+		}
+	putchar('\n');
+
+	return (0);
+}
