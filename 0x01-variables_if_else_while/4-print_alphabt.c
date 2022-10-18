d90adf445dc75d9e4ad86cad101c84a8544b4f32
mutugi001 <collinsmut04@gmail.com>
2022-10-18 10:23:17 -0700

    except

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if ((ch != 'e' && ch != 'q') && ch <= 'z')
-			putchar(ch);
-			ch++;
+	if ((ch != 'e' && ch != 'q') && ch <= 'z')
+	putchar(ch);
+	ch++;
 	}
 	putchar('\n');
 	return (0);
