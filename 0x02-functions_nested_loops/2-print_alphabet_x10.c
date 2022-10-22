7d42fe132fb8994449584cbdd1e329c6c2874a1b
mutugi001 <collinsmut04@gmail.com>
2022-10-22 08:48:31 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet_x10- prints 10 times the alphabet
- * Return:Always 0
  */
 void print_alphabet_x10(void)
 {
@@ -14,5 +13,5 @@ void print_alphabet_x10(void)
 
 	t++;
 	_putchar('\n');
-	return(0);
+	
 }
