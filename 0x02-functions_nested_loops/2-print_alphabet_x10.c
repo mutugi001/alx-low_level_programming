66a0be23f812d1fde8b487157c71502806f51b9d
mutugi001 <collinsmut04@gmail.com>
2022-10-22 08:44:40 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int t = 0
+	int t = 0;
 	char la;
 
-	while(t < 10)
-	{	for(la = 'a'; la <= 'z'; la++)
+	while (t < 10)
+		for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
-	}
+
 	t++;
 	_putchar('\n');
-	Return (0);
-}	
+	Return(0);
+}
