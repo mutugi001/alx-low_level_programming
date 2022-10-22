21b8fa2339b8ec168e166adbdac8affc37d94657
mutugi001 <collinsmut04@gmail.com>
2022-10-22 08:35:13 -0700

    task3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints 10 times the alphabet
+ * Return:Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int t = 0
+	char la;
+
+	while(t < 10)
+	{	for(la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	}
+	t++;
+	_putchar('\n');
+	Return (0);
+}	
