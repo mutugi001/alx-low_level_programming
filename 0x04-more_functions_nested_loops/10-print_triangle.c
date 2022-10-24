00c8047ef84305474577504bf83d728314bc6f6b
mutugi001 <collinsmut04@gmail.com>
2022-10-24 05:05:05 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		{
 			for (tri = size - h; tri > 0; tri--)
 				_putchar(' ');
-			for (ti = 0; tri < h; tri++)
+			for (tri = 0; tri < h; tri++)
 				_putchar('#');
 
 			if (h == size)
