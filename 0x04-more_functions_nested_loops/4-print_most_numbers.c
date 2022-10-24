ba755d7b8312b50944779d42aa50e2ee683f5a59
mutugi001 <collinsmut04@gmail.com>
2022-10-24 02:23:04 -0700

    print nums except

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers- prints nums except 1 and 4
+ * Return:Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '4' && a != '2')
+		{
+			_putchar(num);
+		}
+	}
+
+		_putchar('\n');
+}
