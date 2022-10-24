e41a8ade1f884a2185ad96298ec023cf724d7a1f
mutugi001 <collinsmut04@gmail.com>
2022-10-24 01:59:59 -0700

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers- print numbers 1 to 9
+ * Return:Always 0
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar ('\n');
+
+	return (0);
+}
