3689ab392cfd408a453e027e3fd0697a15b4220b
mutugi001 <collinsmut04@gmail.com>
2022-10-26 03:32:24 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen- returns length of a string
+ * @s: the string
+ * Return:length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*S != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
