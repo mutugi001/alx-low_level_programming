ffa1889bc6371c4976751fa80f5d3d09a7a11d15
mutugi001 <collinsmut04@gmail.com>
2022-11-03 08:31:48 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- hat prints a string, followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	return (s);
+	return ('\n');
+}
