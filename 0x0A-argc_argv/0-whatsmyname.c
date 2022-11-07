5b472480007df07b71b57cf18ce03628b10a3330
mutugi001 <collinsmut04@gmail.com>
2022-11-07 11:18:37 -0800

    printname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- entry point
+ * @argc:arguement count
+ * @argv:arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,10 @@
+#include <unist.h>
+/**
+ * _putchar- writes character to stdout
+ * @c:char to be printed
+ * Return:on success 1
+ */
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
