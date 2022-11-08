43f9085b864e8fca2d4075076b5b0dff80a16cfd
mutugi001 <collinsmut04@gmail.com>
2022-11-08 09:52:13 -0800

    copy string array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - count array
+ * @s:array of elements
+ * Return:1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src:source array
+ * @dest:destination array
+ * Return:dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strdup- returne a pointer to a string
+ * @str:string
+ * Return:pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) + 1;
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dst, str);
+	return (dst);
+}
