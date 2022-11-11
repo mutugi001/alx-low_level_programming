ccdeba220e79cc0de3b644910fbcf5c19327acc5
mutugi001 <collinsmut04@gmail.com>
2022-11-11 13:37:04 -0800

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,15 +16,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[size1] != '\0')
-		size1++;
-	while (s2[size2] != '\0')
-		size2++;
 
-	if (n >= size2)
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
 	{
 		n = size2;
 		p = malloc((size1 + n + 1) * sizeof(char));
@@ -32,13 +38,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i <= size1; i++)
+	for (i = 0; i < size1; i++)
 	{
 		p[i] = s1[i];
 	}
-	for (; i <= size1 + n; i++)
+	for (; i < (size1 + n); i++)
 	{
-		p[i] = s2[0];
+		p[i] = s2[i - size1];
 	}
 	p[i] = '\0';
 
