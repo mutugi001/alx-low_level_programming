769b80d24bd12a32859e9d69f682aa536fcc7685
mutugi001 <collinsmut04@gmail.com>
2022-11-11 12:59:04 -0800

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[size2] != '\0')
 		size2++;
 
-	if (n >= size2)
+	if (n > size2)
 	{
 		n = size2;
 		p = malloc((size1 + n + 1) * sizeof(char));
