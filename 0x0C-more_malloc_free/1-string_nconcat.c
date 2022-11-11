23a4743a4b0aee38c42768bc450c1c6d851c2b5f
mutugi001 <collinsmut04@gmail.com>
2022-11-11 12:46:13 -0800

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,13 +32,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (p == NULL)
 		return (0);
-	for (i = 0; i < size1; i++)
+	for (i = 0; i <= size1; i++)
 	{
 		p[i] = s1[i];
 	}
-	for (; i < size1 + n; i++)
+	for (; i <= size1 + n; i++)
 	{
-		p[i] = s2[i - size1];
+		p[i] = s2[i - size1 - 1];
 	}
 	p[i] = '\0';
 
