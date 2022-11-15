699a0b8bec75d0c93a5fffee1bc535e03a41156b
mutugi001 <collinsmut04@gmail.com>
2022-11-15 14:26:23 -0800

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -30,11 +30,11 @@ void print_dog(struct dog *d)
 
 	if (d->owner != NULL)
 	{
-		printf("Owner: %s", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 	else
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 }
 
