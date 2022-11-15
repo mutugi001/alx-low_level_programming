06daa79bbad93450b4d8874ecca9e6795797af6c
mutugi001 <collinsmut04@gmail.com>
2022-11-15 15:57:34 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -54,6 +54,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (dog1->name == NULL)
 	{
+		free(dog1->name);
 		free(dog1);
 		return (NULL);
 	}
