b33c46c07bf2e2b98c832164f680241a70efa8e6
mutugi001 <collinsmut04@gmail.com>
2022-11-15 13:22:55 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef "DOG_H"
+#define "DOG_H"
 
 #include <stdio.h>
+/**
+ * struct dog- a struct with dog details
+ * @name:name of dog
+ * @age:age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char  *owner;
 };
-
 #endif
