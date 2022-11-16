0e08891ae487e6b6a6c1bb20e7902f642c2c259c
mutugi001 <collinsmut04@gmail.com>
2022-11-16 02:58:19 -0800

    define

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar- writes to stdout
+ * @c:character to print
+ *Return:Always 0
+ */
+ int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <0-object_like_macro.h>
+
+
+int _putchar(char c);
+#endif
