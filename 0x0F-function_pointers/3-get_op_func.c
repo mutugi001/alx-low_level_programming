cce7845b66a9a2bf05ae6c4b71654d3454f5c73b
mutugi001 <collinsmut04@gmail.com>
2022-11-17 14:59:02 -0800

    calc

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "calc.h"
+/**
+ * get_op_func- gets the correct function
+ * @s:operator inputed
+ * Return:pointer to function that corresponds to it
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "calc.h"
+/**
+ * op_add-adds two int
+ * @a:first int
+ * @b:second int
+ * Return:sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- subracts a from b
+ * @a:first int
+ * @b:second int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- multplies two int
+ * @a:first int
+ * @b:second int
+ * Return:product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div-divides a by b
+ * @a:first int
+ * @b:second int
+ * Return:quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- finds the remainder
+ * @a:first int
+ * @b:second int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
