8084c77c4e5c209bb4482063a6f0d7b7e1a4a68a
mutugi001 <collinsmut04@gmail.com>
2022-11-17 13:38:20 -0800

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,8 +7,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	unsigned int i = 0;
 
+	if(!array || !action)
+		return;
+	
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
