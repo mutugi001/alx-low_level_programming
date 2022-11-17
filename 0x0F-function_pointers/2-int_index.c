2a71e49dc98c470e2cb1a99abf0cb7bc7f6e1483
mutugi001 <collinsmut04@gmail.com>
2022-11-17 14:01:50 -0800

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!cmp || !array)
-		return (-1);
-
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
 			return (i);
+		}
 	}
+	return (-1);
 }
