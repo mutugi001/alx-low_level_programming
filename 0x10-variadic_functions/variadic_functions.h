f1ecb549ba1de7ce518447afd708c3d545b90f79
mutugi001 <collinsmut04@gmail.com>
2022-11-18 14:28:29 -0800

    sum

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,6 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 #endif
