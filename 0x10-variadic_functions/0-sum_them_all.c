45a48a3f79a82edb0407e13f6afb4c67d5a73f16
mutugi001 <collinsmut04@gmail.com>
2022-11-18 14:19:44 -0800

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all- sum of all parameters given
+ * @n:amount of arguements
+ * Return:sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	if (num == 0)
+	{
+		return (0);
+	}
+	va_list(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
