df9c72318c8d93ec8b2a7ba81580f2d203e825fb
mutugi001 <collinsmut04@gmail.com>
2022-11-30 07:23:58 -0800

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,15 +3,7 @@
 
 #include <stdlib.h>
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
-
-/**
+/** 
  * struct list_s- singly linked list
  * @str:string- (malloc'ed string)
  * @len: length of string
@@ -27,4 +19,5 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 #endif
